b93332ac4d65fab00b26b009fc014b6f173715b5
DeveloperLuciaH <85319857+DeveloperLuciaH@users.noreply.github.com>
2021-07-24 16:28:36 -0300

    C# -  Proeto de Apólice de Seguro Automóvel

---
+++ Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace prj_ApoliceLucia
+{
+    partial class FrmApolice
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.rdMasculino = new System.Windows.Forms.RadioButton();
+            this.rdFeminino = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.grPagamento = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbParcela = new System.Windows.Forms.ComboBox();
+            this.lstDetalhamento = new System.Windows.Forms.ListBox();
+            this.lblValorPagar = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.grPagamento.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rdFeminino);
+            this.groupBox1.Controls.Add(this.rdMasculino);
+            this.groupBox1.Controls.Add(this.txtIdade);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Location = new System.Drawing.Point(77, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 137);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Condutor";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(6, 32);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(315, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Idade";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(8, 87);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(108, 20);
+            this.txtIdade.TabIndex = 3;
+            // 
+            // rdMasculino
+            // 
+            this.rdMasculino.AutoSize = true;
+            this.rdMasculino.Location = new System.Drawing.Point(142, 87);
+            this.rdMasculino.Name = "rdMasculino";
+            this.rdMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rdMasculino.TabIndex = 4;
+            this.rdMasculino.TabStop = true;
+            this.rdMasculino.Text = "Masculino";
+            this.rdMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rdFeminino
+            // 
+            this.rdFeminino.AutoSize = true;
+            this.rdFeminino.Location = new System.Drawing.Point(221, 88);
+            this.rdFeminino.Name = "rdFeminino";
+            this.rdFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rdFeminino.TabIndex = 5;
+            this.rdFeminino.TabStop = true;
+            this.rdFeminino.Text = "Feminino";
+            this.rdFeminino.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sexo";
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Items.AddRange(new object[] {
+            "Volkswagem",
+            "Chevrolet",
+            "Fiat",
+            "Ford",
+            "Nissan",
+            "Citroen",
+            "Ferrari",
+            "Porshe",
+            "Mercedes Benz",
+            ""});
+            this.cmbMarca.Location = new System.Drawing.Point(6, 45);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(181, 21);
+            this.cmbMarca.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtValor);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cmbMarca);
+            this.groupBox2.Location = new System.Drawing.Point(77, 168);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 87);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Automóvel";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Marca";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(200, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Valor";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(203, 45);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(118, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblValorPagar);
+            this.groupBox3.Controls.Add(this.btnCalcular);
+            this.groupBox3.Location = new System.Drawing.Point(77, 261);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(327, 69);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Valor a pagar";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(6, 19);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(110, 29);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // grPagamento
+            // 
+            this.grPagamento.Controls.Add(this.lstDetalhamento);
+            this.grPagamento.Controls.Add(this.cmbParcela);
+            this.grPagamento.Controls.Add(this.label6);
+            this.grPagamento.Location = new System.Drawing.Point(77, 353);
+            this.grPagamento.Name = "grPagamento";
+            this.grPagamento.Size = new System.Drawing.Size(327, 181);
+            this.grPagamento.TabIndex = 4;
+            this.grPagamento.TabStop = false;
+            this.grPagamento.Text = "Pagamento";
+            this.grPagamento.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Qtde. Parcela";
+            // 
+            // cmbParcela
+            // 
+            this.cmbParcela.FormattingEnabled = true;
+            this.cmbParcela.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10"});
+            this.cmbParcela.Location = new System.Drawing.Point(92, 22);
+            this.cmbParcela.Name = "cmbParcela";
+            this.cmbParcela.Size = new System.Drawing.Size(121, 21);
+            this.cmbParcela.TabIndex = 1;
+            this.cmbParcela.SelectedIndexChanged += new System.EventHandler(this.cmbParcela_SelectedIndexChanged);
+            // 
+            // lstDetalhamento
+            // 
+            this.lstDetalhamento.FormattingEnabled = true;
+            this.lstDetalhamento.Location = new System.Drawing.Point(8, 58);
+            this.lstDetalhamento.Name = "lstDetalhamento";
+            this.lstDetalhamento.Size = new System.Drawing.Size(207, 108);
+            this.lstDetalhamento.TabIndex = 2;
+            this.lstDetalhamento.SelectedIndexChanged += new System.EventHandler(this.lstDetalhamento_SelectedIndexChanged);
+            // 
+            // lblValorPagar
+            // 
+            this.lblValorPagar.AutoSize = true;
+            this.lblValorPagar.Location = new System.Drawing.Point(144, 16);
+            this.lblValorPagar.Name = "lblValorPagar";
+            this.lblValorPagar.Size = new System.Drawing.Size(0, 13);
+            this.lblValorPagar.TabIndex = 1;
+            // 
+            // FrmApolice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 537);
+            this.Controls.Add(this.grPagamento);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmApolice";
+            this.Text = "FrmApolice";
+            this.Load += new System.EventHandler(this.FrmApolice_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.grPagamento.ResumeLayout(false);
+            this.grPagamento.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdFeminino;
+        private System.Windows.Forms.RadioButton rdMasculino;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblValorPagar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox grPagamento;
+        private System.Windows.Forms.ListBox lstDetalhamento;
+        private System.Windows.Forms.ComboBox cmbParcela;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prj_ApoliceLucia
+{
+    public partial class FrmApolice : Form
+    {
+        char sexo;
+        int idade;
+        double valorAutomovel;
+        double valorApolice;
+
+        public FrmApolice()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmApolice_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstDetalhamento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbParcela_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstDetalhamento.Items.Clear();
+
+            int parcela = Convert.ToInt32(cmbParcela.Text);
+
+            for(int x = 1; x <= parcela; x++)
+            {
+                lstDetalhamento.Items.Add(x + " - " + "Venc. "
+                + DateTime.Now.AddMonths(x - 1).ToString("dd/MM/yyyy") + " =>" + (valorApolice / parcela).ToString("c2"));
+            }
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtNome.Text))
+                {
+                    throw new Exception("Nome Inválido");
+                }
+                if (string.IsNullOrEmpty(cmbMarca.Text))
+                {
+                    throw new Exception("Marca Inválida");
+                }
+
+                lstDetalhamento.Items.Clear();
+
+                //recuperar valores da tela
+                idade = Convert.ToInt16(txtIdade.Text);
+                valorAutomovel = Convert.ToDouble(txtValor.Text);
+                if (rdMasculino.Checked)
+                    sexo = 'M';
+                else
+                    sexo = 'F';
+
+                //calcular valor da apólice
+                double valorApolice = 0;
+                if (sexo == 'M' && idade <= 25)
+                    valorApolice = valorAutomovel * 0.1;
+                if (sexo == 'M' && idade > 25)
+                    valorApolice = valorAutomovel * 0.05;
+                if (sexo == 'F')
+                    valorApolice = valorAutomovel * 0.02;
+
+                // mostrar na tela o resultado
+                this.valorApolice = valorApolice;
+                lblValorPagar.Text = valorApolice.ToString("c2");
+                grPagamento.Visible = true;
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("Limite da variável estourou - " + ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show("Formato incorreto -" + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Genérico - " + ex.Message);
+            }
+            finally
+            {
+                if (string.IsNullOrEmpty(lblValorPagar.Text))
+                    lblValorPagar.Text = "0,00";
+            }
+
+        }
+
+
+        
+    }
+}
